6ac88e43ddb8473472c99594de89ea837388cd78
gauravkharel <kgaurav21@tbc.edu.np>
2021-11-11 20:33:38 +0545

    created product model

---
+++ backend/model/product.js
@@ -0,0 +1,114 @@
+const mongoose = require(mongoose);
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true,
+        maxlength: 5,
+        default: 0.00,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            },
+            
+        }
+    ],        
+    category: {
+        type: String,
+        required: true,
+        enum: {
+            values: [
+                'clothing', 
+                'accessories', 
+                'shoes', 
+                'bags', 
+                'others'
+            ],
+            message: 'Please select the mentioned category.'
+        }
+    },
+    seller: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+        maxlength: 5,
+        default: 0
+    },
+    numOfRatings: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true,
+                maxlength: 5,
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    color: {
+        type: String,
+        required: true,
+        enum: {
+            values: [
+                'black',
+                'white',
+                'red',
+                'blue',
+                'green',
+                'yellow',
+                'orange',
+                'purple',
+                'pink',
+                'brown',
+                'grey',
+                'silver',
+                'gold',
+                'others'
+            ],
+            message: 'Please select through the option listed.'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+    
+exports = mongoose.model('Product', productSchema); 
+
+
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {} = require('../controllers/productController');
 const { getProducts } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/products/new').post(newProduct);
 
 module.exports = router;
