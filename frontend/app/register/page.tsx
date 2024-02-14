9c7f91e1b7b36b3349d02a9bb2fe8b6d70d220c4
gauravkharel <kharel619@gmail.com>
2024-02-14 13:51:43 +0545

    Change frontend to NextJS

---
+++ frontend/app/register/page.tsx
@@ -0,0 +1,13 @@
+import { ProfileForm } from "@/components/exampleform"
+
+interface pageProps {
+  
+}
+
+const page = ({}) => {
+  return <div>
+    <ProfileForm />
+  </div>
+}
+
+export default page
