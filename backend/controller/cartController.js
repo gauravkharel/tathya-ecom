72ca2cd16e4fea3ef8882a4341cffa179b05910a
gauravkharel <kharel619@gmail.com>
2024-06-09 15:11:52 +0545

    Create cart route

---
+++ backend/controller/cartController.js
@@ -0,0 +1,27 @@
+const prisma = require('../lib/db')
+
+const addProductToCart = async (req, res) => {
+    const {productId, cartId} = req.body
+}
+
+const getCarts = async (req, res) => {
+
+}
+
+const clearCart = async(req, res) => {
+
+}
+
+const removeCartItem = async (req, res) => {
+
+}
+
+const deleteAllCarts = async(req, res) => {
+
+}
+
+const updateCartItem = async(req, res) => {
+
+}
+
+
