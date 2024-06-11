090885b71f0cc78644e846a931e7f7df4d41e047
gauravkharel <kharel619@gmail.com>
2024-06-11 14:10:34 +0545

    Adding new controller

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -1,12 +1,82 @@
 const prisma = require("../lib/db");
 
-const getCarts = async (req, res) => {};
+const getCarts = async (req, res) => {
+  try {
+    const cart = await prisma.cart.findMany({
+      include: {
+        clothing: true,
+        users: true,
+      },
+    });
+    res.json(cart);
+    console.log("Fetched cart", cart);
+  } catch (err) {
+    res.status(402).json({ message: "The cart is empty." });
+  }
+};
 
-const getCartItem = async (req, res) => {};
+const getCartsByUserId = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const cart = await prisma.cart.findMany({
+      where: {
+        users: userId,
+      },
+      include: {
+        clothing: true,
+        users: true,
+      },
+    });
+    res.json(cart);
+    console.log("Fetched cart", cart);
+  } catch (err) {
+    res.status(402).json({ message: "The cart is empty." });
+  }
+};
+
+const getCartItem = async (req, res) => {
+  try {
+    const { cartId } = req.body;
+    const cartItem = await prisma.cart.findUnique({
+      where: {
+        cartId: cartId,
+      },
+      include: {
+        clothing: true,
+        users: true,
+      },
+    });
+
+    res.json(cartItem);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 const addProductToCart = async (req, res) => {
-  const { productId, cartId } = req.body;
+  try {
+    const { productId, userId, quantity } = req.body;
+
+    const productToCart = await prisma.cart.create({
+      data: {
+        clothing: productId,
+        users: userId,
+        quantity: quantity,
+      },
+      include: {
+        clothing: true,
+        users: true,
+      },
+    });
+
+    res.json(productToCart)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
+
 const deleteCartItem = async (req, res) => {};
 
 const deleteCarts = async (req, res) => {};
@@ -16,6 +86,8 @@ const updateCartItem = async (req, res) => {};
 module.exports = {
   addProductToCart,
   getCarts,
+  getCartItem,
+  getCartsByUserId,
   deleteCartItem,
   deleteCarts,
   updateCartItem,
