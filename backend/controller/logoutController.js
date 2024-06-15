d4e06499293c5ba0d3e6ba07a11343e0870ea978
gauravkharel <kharel619@gmail.com>
2024-06-15 17:49:48 +0545

    adding logout controller

--- backend/controller/logoutController.js
+++ backend/controller/logoutController.js
@@ -1,5 +1,36 @@
-const handleLogout = async({req, res}) => {
-    
-}
+const process = require("process");
+const prisma = require("../lib/db");
+require("dotenv").config();
 
-module.exports = handleLogout
+const handleLogout = async ({ req, res }) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); 
+    const refreshToken = cookies.jwt;
+
+
+  const foundUser = await prisma.user.findFirst({
+    where: {
+      refreshToken: {
+        token: refreshToken,
+      },
+    },
+  });
+
+  if (!foundUser) {
+    res.clearCookies("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    return res.sendStatus(204);
+  }
+
+  const deleteToken = await prisma.refreshToken.delete({
+    where: {
+      user: {
+        email: foundUser.email,
+      },
+    },
+  });
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
